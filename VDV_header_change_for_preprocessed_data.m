81b1f436d9e2c348dc4786e3b10c8548b2a024f6
kdolgikh <kdolgikh@alaska.edu>
2019-02-12 13:56:28 -0900

    Added a lookup table function to change site codes for processed data sets

--- VDV_header_change_for_preprocessed_data.m
+++ VDV_header_change_for_preprocessed_data.m
@@ -1,3 +1,6 @@
+% This script will be used as it is only for 2018 data since next year I will be
+% working with unprocessed data from VDV
+
 clear
 clc
 
@@ -21,78 +24,109 @@ if ~exist(folder_modified,'dir')
     mkdir(folder,'modified')
 end
 
+% Ask a user to provide a lookup table
+prompt = 'Enter the name of the lookup table file:\n';
+lookup_table_name = input(prompt,'s');
+
+fid=fopen(lookup_table_name);
+lookup_t = textscan(fid,'%s %s','Delimiter',',');
+fclose(fid);
+
+lookup_table=[];
+for i=1:length(lookup_t)
+    table_next=lookup_t{1,i};
+    lookup_table=[lookup_table, table_next];
+end
+
 % z=0;
 
 for j = 1:length(files)
    
     if ~exist(fullfile(folder_modified,files(j).name),'file')
        
-        fid=fopen(fullfile(folder,files(j).name));
-
-        % read lines until get to the line with column header
-        for i=1:header_start_line
-            data_header = fgetl(fid);
-        end
-        data_header = strsplit(data_header,',');
-        num_columns = length(data_header);
-
-        % create a string to be used in textscan to read data
-        data_string = '%s';
-        for i=1:num_columns
-            data_string = strcat(data_string,' %s');
-        end
-
-        table_data = textscan(fid,data_string,'Delimiter',',');
-
-        fclose(fid);
-
-        data_header{1}='Date';
-        depths=zeros(1,num_columns);
-        depths_string=strings(1,num_columns);
+        errmsg='';
+        [fid,errmsg]=fopen(fullfile(folder,files(j).name));
+ 
+        if fid>0
         
-        % add '+' sign for all positive depths
-        for i=2:num_columns
-            if strcmp(data_header{i}(6:7),'TS')||...    % thermistor string
-                strcmp(data_header{i}(6:7),'TP')        % thermistor probe
-                data_header{i}=strcat(data_header{i}(1:6),'H',data_header{i}(7:end));
-            else
-                if strcmp(data_header{i}(6:7),'HP') % hydra probe
-                   data_header{i}=strcat(data_header{i}(1:6),'Y',data_header{i}(7:end)); 
-                end
+            % read lines until get to the line with column header
+            for i=1:header_start_line
+                data_header = fgetl(fid);
+            end
+            data_header = strsplit(data_header,',');
+            num_columns = length(data_header);
+
+            % create a string to be used in textscan to read data
+            data_string = '%s';
+            for i=1:num_columns
+                data_string = strcat(data_string,' %s');
             end
 
-            if strcmp(data_header{i}(10),'A')
-                data_header{i}(10)='a';
-            else
-                if  strcmp(data_header{i}(10),'S')
-                    data_header{i}(10)='s';
+            table_data = textscan(fid,data_string,'Delimiter',',');
+
+            fclose(fid);
+            
+            data_header{1}='Date';
+            depths=zeros(1,num_columns);
+            depths_string=strings(1,num_columns);
+
+            % add '+' sign for all positive depths
+            for i=2:num_columns
+                if strcmp(data_header{i}(6:7),'TS')||...    % thermistor string
+                    strcmp(data_header{i}(6:7),'TP')        % thermistor probe
+                    data_header{i}=strcat(data_header{i}(1:6),'H',data_header{i}(7:end));
                 else
-                    depths(i)=str2double(data_header{i}(10:(end-1)));
-                    depths_string(i)=generate_depth_string(depths(i));
-                    data_header{i}=strcat(data_header{i}(1:9),depths_string(i),'m');
+                    if strcmp(data_header{i}(6:7),'HP') % hydra probe
+                       data_header{i}=strcat(data_header{i}(1:6),'Y',data_header{i}(7:end)); 
+                    end
+                end
+
+                if strcmp(data_header{i}(10),'A')
+                    data_header{i}(10)='a';
+                else
+                    if  strcmp(data_header{i}(10),'S')
+                        data_header{i}(10)='s';
+                    else
+                        depths(i)=str2double(data_header{i}(10:(end-1)));
+                        depths_string(i)=generate_depth_string(depths(i));
+                        data_header{i}=strcat(data_header{i}(1:9),depths_string(i),'m');
+                    end
                 end
             end
+
+            table_data_temp=[];
+            for i=1:length(table_data)
+                table_next=table_data{1,i};
+                table_data_temp=[table_data_temp, table_next];
+            end
+
+            % This piece of code helped to determine that every file has an extra
+            % column with null character ''
+    %         s=size(table_data_temp);
+    %         if s(2)>length(data_header)
+    %             z=z+1; %counts files with the extra column
+    %         end
+            % Therefore, it is safe to remove the last column
+            table_data_temp(:,end)=[];
+
+            table_data = [data_header;table_data_temp];
+
+            new_site_code = lookup_site_name(files(j).name(1:3),lookup_table);
+
+            files(j).name = strcat(new_site_code,files(j).name(4:end));
+
+            files(j).name(17:21)=[];
+
+            files(j).name = strcat(files(j).name(1:10),'_',table_data{2,1},...
+                '_',table_data{end,1},files(j).name(11:end));
+
+            writetable(cell2table(table_data),fullfile(folder_modified,files(j).name),...
+            'WriteVariableNames',false);
+        
+        else
+            disp(' ');
+            disp(errmsg);
         end
-        
-        table_data_temp=[];
-        for i=1:length(table_data)
-            table_next=table_data{1,i};
-            table_data_temp=[table_data_temp, table_next];
-        end
-        
-        % This piece of code helped to determine that every file has an extra
-        % column with null character ''
-%         s=size(table_data_temp);
-%         if s(2)>length(data_header)
-%             z=z+1; %counts files with the extra column
-%         end
-        % Therefore, it is safe to remove the last column
-        table_data_temp(:,end)=[];
-        
-        table_data = [data_header;table_data_temp];
-        
-        writetable(cell2table(table_data),fullfile(folder_modified,files(j).name),...
-        'WriteVariableNames',false);
         
     end
     
---
+++ lookup_site_name.m
@@ -0,0 +1,21 @@
+function [site_name_new] = lookup_site_name(site_name_old, lookup_table)
+% This functions looks for a pair old name - new name and returns the new
+% name value
+
+    site_name_new = 'not_found';
+    
+    for i=1:length(lookup_table)
+        if strcmp(lookup_table(i,1),site_name_old)
+            site_name_new = lookup_table{i,2};
+            break;
+        end
+    end
+    
+    if strcmp(site_name_new,'not_found')
+        disp(' ');
+        disp(['No match is found for the site name ',site_name_old,' in the provided lookup table']);
+        site_name_new = site_name_old;
+    end
+    
+end
+
